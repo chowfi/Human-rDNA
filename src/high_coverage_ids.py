6da547cc876a118c2abc2a7bb377592182377140
chowfi <fc1132+github@nyu.edu>
2024-06-06 17:54:01 -0400

    update src to save output

--- src/high_coverage_ids.py
+++ src/high_coverage_ids.py
@@ -1,12 +1,26 @@
 import re
+import csv
 
-# Path to your CSV file
-file_path = '/Users/fionachow/Documents/NYU/CDS/Summer 2024/Human rDNA Research/Project/Human-rDNA/original reference/1000data.csv'
+def main():
+    file_path = '/Users/fionachow/Documents/NYU/CDS/Summer 2024/Human rDNA Research/Project/Human-rDNA/original reference/1000data.csv'
 
-# This will hold all matches
-matching_entries = []
+    matching_entries = []
 
-with open(file_path, 'r') as file:
-    for line in file:
-        matches = re.findall(r',(ERR\w*)', line)
-        matching_entries.extend(matches)
+    with open(file_path, 'r') as file:
+        for line in file:
+            match = re.search(r',(ERR\w*)', line)
+            if match:  
+                matching_entries.append(match.group(1)) 
+
+    print("Found matches:", matching_entries)
+    print("Number of matches:", len(matching_entries))
+
+    with open('/Users/fionachow/Documents/NYU/CDS/Summer 2024/Human rDNA Research/Project/Human-rDNA/outputs/high_coverage_ids.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['High Coverage Entries'])
+        # Write each match on a new line
+        for match in matching_entries:
+            csvwriter.writerow([match])
+            
+if __name__ == "__main__":
+    main()
