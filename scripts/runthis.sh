427b04c69a8a391c2141aed5bd74c65a2abbcd1d
chowfi <fc1132+github@nyu.edu>
2025-05-07 22:04:13 -0400

    fix og vcf files that did not re-dld previously on Nov 7, 2024

--- scripts/runthis.sh
+++ scripts/runthis.sh
@@ -2,8 +2,8 @@
 
 USER="fc1132"
 HPC_HOST="greene.hpc.nyu.edu"
-TXT_FILE="/Users/fionachow/Documents/NYU/CDS/Summer 2024/Human rDNA Research/Project/Human-rDNA/outputs/scripts_outputs/vcf_files_all.txt"
-LOCAL_DIR="/Users/fionachow/Documents/NYU/CDS/Summer 2024/Human rDNA Research/Project/Human-rDNA/original reference/original_vcf_all"
+TXT_FILE="/Users/fionachow/Documents/NYU/CDS/Summer 2024/Human rDNA Research/Project/Human-rDNA/outputs/scripts_outputs/vcf_files_fix.txt"
+LOCAL_DIR="/Users/fionachow/Documents/NYU/CDS/Summer 2024/Human rDNA Research/Project/Human-rDNA/original reference/original_vcf_fix"
 
 if [ ! -f "$TXT_FILE" ]; then
     echo "File $TXT_FILE does not exist on the local system."
